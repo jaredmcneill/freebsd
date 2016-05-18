0f66e11a5828a36951d851a6fb3e27a5e9e366dc
Jared McNeill <jmcneill@invisible.ca>
2016-05-17 21:19:33 -0300

    Switch to extres phy API

--- sys/arm/allwinner/aw_usbphy.c
+++ sys/arm/allwinner/aw_usbphy.c
@@ -47,8 +47,11 @@ __FBSDID("$FreeBSD$");
 #include <dev/extres/clk/clk.h>
 #include <dev/extres/hwreset/hwreset.h>
 #include <dev/extres/regulator/regulator.h>
+#include <dev/extres/phy/phy.h>
 
-#define	USBPHY_NUMOFF		3
+#include "phy_if.h"
+
+#define	USBPHY_NPHYS	4
 
 static struct ofw_compat_data compat_data[] = {
 	{ "allwinner,sun4i-a10-usb-phy",	1 },
@@ -60,14 +63,21 @@ static struct ofw_compat_data compat_data[] = {
 	{ NULL,					0 }
 };
 
+struct awusbphy_softc {
+	regulator_t	reg[USBPHY_NPHYS];
+};
+
 static int
 awusbphy_init(device_t dev)
 {
+	struct awusbphy_softc *sc;
 	char pname[20];
 	int error, off;
 	regulator_t reg;
 	hwreset_t rst;
 	clk_t clk;
+
+	sc = device_get_softc(dev);
 
 	/* Enable clocks */
 	for (off = 0; clk_get_by_ofw_index(dev, off, &clk) == 0; off++) {
@@ -89,17 +99,41 @@ awusbphy_init(device_t dev)
 		}
 	}
 
-	/* Enable regulator(s) */
-	for (off = 0; off < USBPHY_NUMOFF; off++) {
+	/* Get regulators */
+	for (off = 0; off < USBPHY_NPHYS; off++) {
 		snprintf(pname, sizeof(pname), "usb%d_vbus-supply", off);
-		if (regulator_get_by_ofw_property(dev, pname, &reg) != 0)
-			continue;
+		if (regulator_get_by_ofw_property(dev, pname, &reg) == 0)
+			sc->reg[off] = reg;
+	}
+
+	return (0);
+}
+
+static int
+awusbphy_phy_enable(device_t dev, int phy, bool enable)
+{
+	struct awusbphy_softc *sc;
+	regulator_t reg;
+	int error;
+
+	if (phy < 0 || phy >= USBPHY_NPHYS)
+		return (ERANGE);
+
+	sc = device_get_softc(dev);
+
+	/* Regulators are optional. If not found, return success. */
+	reg = sc->reg[phy];
+	if (reg == NULL)
+		return (0);
+
+	if (enable)
 		error = regulator_enable(reg);
-		if (error != 0) {
-			device_printf(dev, "couldn't enable regulator %s\n",
-			    pname);
-			return (error);
-		}
+	else
+		error = regulator_disable(reg);
+	if (error != 0) {
+		device_printf(dev, "couldn't %s regulator for phy %d\n",
+		    enable ? "enable" : "disable", phy);
+		return (error);
 	}
 
 	return (0);
@@ -124,9 +158,13 @@ awusbphy_attach(device_t dev)
 	int error;
 
 	error = awusbphy_init(dev);
-	if (error)
+	if (error) {
 		device_printf(dev, "failed to initialize USB PHY, error %d\n",
 		    error);
+		return (error);
+	}
+
+	phy_register_provider(dev);
 
 	return (error);
 }
@@ -136,16 +174,20 @@ static device_method_t awusbphy_methods[] = {
 	DEVMETHOD(device_probe,		awusbphy_probe),
 	DEVMETHOD(device_attach,	awusbphy_attach),
 
+	/* PHY interface */
+	DEVMETHOD(phy_enable,		awusbphy_phy_enable),
+
 	DEVMETHOD_END
 };
 
 static driver_t awusbphy_driver = {
 	"awusbphy",
 	awusbphy_methods,
-	0,
+	sizeof(struct awusbphy_softc)
 };
 
 static devclass_t awusbphy_devclass;
 
-DRIVER_MODULE(awusbphy, simplebus, awusbphy_driver, awusbphy_devclass, 0, 0);
+EARLY_DRIVER_MODULE(awusbphy, simplebus, awusbphy_driver, awusbphy_devclass,
+    0, 0, BUS_PASS_RESOURCE + BUS_PASS_ORDER_MIDDLE);
 MODULE_VERSION(awusbphy, 1);
